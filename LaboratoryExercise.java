3b6d4b6c32611faf62135ca0016ae18eb287b2a0
Antoinette Buligan <antoinettebuligan572@gmail.com>
2024-11-13 09:45:53 +0800

    Add files via upload

---
+++ LaboratoryExercise.java
@@ -0,0 +1,18 @@
+
+package laboratoryexercise;
+
+public class LaboratoryExercise {
+      public static void main(String[] args) {
+          int iNumber = 3;
+          
+          if (iNumber >= 0) {
+              
+              System.out.println("first string");
+              System.out.println("second string");
+          }
+          
+          else {
+          }
+          System.out.println("third string");
+      }
+}      
